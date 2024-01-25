bb06bbf605f3857ff6a1cecef5d7b01f69363107
kevinngonzales <kevinngstem@gmail.com>
2024-01-24 18:29:37 -0800

    added update and delete functionality

---
+++ public/main.js
@@ -0,0 +1,49 @@
+const update = document.getElementById('update-button');
+
+const deleteButton = document.getElementById('delete-button');
+
+
+
+update.addEventListener('click', _ => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch('/users', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        }),
+    })
+        .then(res => {
+            console.log(`main.js res`, JSON.stringify(res));
+            if (res.ok) return res.json();
+        })
+
+        .then(res =>{
+            window.location.reload(true)
+        })
+        
+})
+
+
+deleteButton.addEventListener('click', () => {
+    const username = document.getElementById("username").value;
+
+    fetch('/users', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: username
+        }),
+    })
+    .then(res => {
+        console.log('deleted')
+        if (res.ok) return res.json();
+    })
+})
+
+
+
+
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 console.log('Is this thing on?');
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = 3000;
 
+
 require('dotenv').config({ path: '.env' });
 
 
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 
@@ -34,6 +35,45 @@ MongoClient.connect(process.env.MONGO_URI)
                 .catch(error => console.error(error))
         })
 
+        app.put('/users', (req, res) => {
+            usersCollection
+                .findOneAndUpdate(
+                    { username: req.body.username },
+                    {
+                        $set: {
+                            username: req.body.username,
+                            password: req.body.password,
+                        },
+                    },
+                    {
+                        upsert: false,
+                    },
+                    {
+                        returnNewDocument: true
+                    }
+                )
+                .then(result => {
+                    res.json('Success')
+                    return res
+                })
+                .then(res => {
+                    window.location.reload(true)
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.delete('/users', (req, res) => {
+            usersCollection
+                .deleteOne(
+                    { username: req.body.username }
+                )
+                .then(result => {
+                    console.log(`Deleted ${req.body.username}`)
+                    console.log(result);
+                    res.json('Deleted user')
+                })
+                .catch(error => console.error(error))
+        })
 
         app.post('/users', (req, res) => {
             usersCollection
